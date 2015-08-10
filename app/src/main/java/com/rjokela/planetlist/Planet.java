884d8a12aa841df1b48ca9ae96c9c7c45ab14ecc
RKJokela <randyjokela@gmail.com>
2015-08-09 21:37:25 -0700

    Add Planet class

---
+++ app/src/main/java/com/rjokela/planetlist/Planet.java
@@ -0,0 +1,20 @@
+package com.rjokela.planetlist;
+
+/**
+ * Created by Randon K. Jokela on 8/9/2015.
+ */
+public class Planet {
+    public int logo;
+    public String name;
+    public String type;
+    public Planet() {
+        super();
+    }
+    public Planet(int logo, String name, String type) {
+        super();
+        this.logo = logo;
+        this.name = name;
+        this.type = type;
+    }
+}
+
